6f5991b7c0455197294a546fc78e757216eb5d30
IDC87 <ivo.diogo@gmail.com>
2023-01-12 10:54:29 +0000

    fixed error where list of 100 and 500 numbers were not correctly generated, added operations count to lists

--- test_leaks.sh
+++ test_leaks.sh
@@ -1,8 +1,83 @@
 #!/bin/bash
 
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+
+
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 GREY='\033[90m'
+CYAN='\033[0;36m'
 BOLD='\033[1m'
 NC='\033[0m'
 GREEN='\033[0;32m'
@@ -48,28 +123,34 @@ function center_text {
 }
 
 declare -a hundred_numbers
-for i in {1..100}; do
-  new_number=$((RANDOM % 201 - 100))
+count=0
+while [ $count -lt 100 ]
+do
+  new_number=$((RANDOM % 301 - 150))
   if [[ " ${hundred_numbers[@]} " =~ " ${new_number} " ]]; then
     continue
   else
     hundred_numbers+=($new_number)
+    count=$((count+1))
   fi
 done
 IFS=' '
 hundred_numbers_string="\"${hundred_numbers[*]}\""
 
 declare -a five_hundred_numbers
-for i in {1..500}; do
-  new_number=$((RANDOM % 501 - 250))
+count=0
+while [ $count -lt 500 ]
+do
+  new_number=$((RANDOM % 1801 - 900))
   if [[ " ${five_hundred_numbers[@]} " =~ " ${new_number} " ]]; then
     continue
   else
     five_hundred_numbers+=($new_number)
+    count=$((count+1))
   fi
 done
 IFS=' '
-five_hundred_numbers_string="\"${hundred_numbers[*]}\""
+five_hundred_numbers_string="\"${five_hundred_numbers[*]}\""
 
 declare -a five_numbers
 count=0
@@ -218,6 +299,15 @@ if [ ! -f "$checker_name" ]; then
   exit 1
 fi
 
+if [ -x "./$checker_name" ]; then
+    echo -e "\n${GREEN}${BOLD}./$checker_name has executable permissions\n"
+else
+    echo -e "\n${RED}${BOLD}Warning: $checker_name does not have executable permissions!\n${NC}"
+	echo -e "${YELLOW}run '"chmod 777 ./$checker_name"' to make it executable\n${NC}"
+	sleep 1
+	exit 1
+fi
+
 sleep 1
 echo -e "\n\n\e[1;30;4;42mPush_swap - simple version\e[0m\n"
 sleep 1
@@ -233,6 +323,7 @@ echo -e "\n"
 ARG="1 5 2 4 3"; ./push_swap $ARG | ./checker $ARG
 echo -e "\n"
 ./push_swap $ARG
+echo -e "\n${UYellow}Number of operations:${NC} ${YELLOW}$(./push_swap $ARG | wc -l | cut -f1 -d ' ')${NC}"
 
 sleep 1
 echo -e "\n\n\n${ITALICRED}ARG="${five_numbers[@]}"; ./push_swap ${five_numbers[@]} | ./checker ${five_numbers[@]}${NC}"
@@ -240,6 +331,7 @@ echo -e "\n"
 ARG="${five_numbers[@]}"; ./push_swap ${five_numbers[@]} | ./checker ${five_numbers[@]}
 echo -e "\n"
 ./push_swap ${five_numbers[@]}
+echo -e "\n${UYellow}Number of operations:${NC} ${YELLOW}$(./push_swap $ARG | wc -l | cut -f1 -d ' ')${NC}"
 
 
 sleep 1
@@ -249,12 +341,15 @@ echo -e "\n${ITALICRED}ARG="${hundred_numbers[@]}"; ./push_swap 100 numbers | ./
 echo -e "\n"
 ARG="${hundred_numbers[@]}"; ./push_swap $ARG | ./checker $ARG
 echo -e "\n"
+echo -e "${UYellow}Number of operations:${NC} ${YELLOW}$(./push_swap $ARG | wc -l | cut -f1 -d ' ')${NC}"
 
 
 sleep 1
-echo -e "\n\n\e[1;30;4;42mPush_swap - Advanced version\e[0m\n"
+echo -e "\n\n\n\e[1;30;4;42mPush_swap - Advanced version\e[0m\n"
 sleep 1
 echo -e "\n${ITALICRED}ARG="${five_hundred_numbers[@]}"; ./push_swap 500 numbers | ./checker 500 numbers${NC}"
 echo -e "\n"
 ARG="${five_hundred_numbers[@]}"; ./push_swap $ARG | ./checker $ARG
 echo -e "\n"
+echo -e "${UYellow}Number of operations:${NC} ${YELLOW}$(./push_swap $ARG | wc -l | cut -f1 -d ' ')${NC}\n\n"
+
