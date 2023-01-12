894ee18d527ec2be1a46367ba04dcf2f68147e6a
IDC87 <ivo.diogo@gmail.com>
2023-01-12 00:50:54 +0000

    optimize the code to be less cluttered, valgrind no more give false/positives if output of program is Error

--- test_leaks.sh
+++ test_leaks.sh
@@ -8,51 +8,32 @@ NC='\033[0m'
 GREEN='\033[0;32m'
 UNDERLINED='\033[4m'
 
-BOLDGREEN="\e[1;${GREEN}m"
+BOLDGREEN="\e[1;${GREEN}"
 ITALICRED="\e[3;${GREY}"
 ENDCOLOR="\e[0m"
 
 file_name="push_swap"
 checker_name="checker"
 
+check_os_and_execute() {
+    if [ -f /etc/os-release ]; then
+        echo -e "\n\e[1;30;4;42mDetected Linux\e[0m"
+    elif [ "$(uname)" == "Darwin" ]; then
+        echo "Detected MacOS"        
+    else
+        echo "Unknown Operating System"
+    fi
+}
+check_os_and_execute
+
+
 if [ ! -f "$file_name" ]; then
   echo -e "\n${RED}Error: \nyou must have the $file_name file created for this to work\n"
   exit 1
 else
   echo -e "\n${GREEN}File $file_name found\n"
-  echo -e "${GREEN}STARTING\n"
+  echo -e "${GREEN}${BOLD}STARTING${NC}\n"
 fi
-
-declare -a hundred_numbers
-for i in {1..100}; do
-  new_number=$((RANDOM % 201 - 100))
-  if [[ " ${hundred_numbers[@]} " =~ " ${new_number} " ]]; then
-    continue
-  else
-    hundred_numbers+=($new_number)
-  fi
-done
-
-declare -a five_hundred_numbers
-for i in {1..500}; do
-  new_number=$((RANDOM % 501 - 250))
-  if [[ " ${five_hundred_numbers[@]} " =~ " ${new_number} " ]]; then
-    continue
-  else
-    five_hundred_numbers+=($new_number)
-  fi
-done
-
-declare -a five_numbers
-for i in {1..5}; do
-  new_number=$((RANDOM % 25 - 12))
-  if [[ " ${five_numbers[@]} " =~ " ${new_number} " ]]; then
-    continue
-  else
-    five_numbers+=($new_number)
-  fi
-done
-
 
 function center_text {
   local text="$1"
@@ -65,149 +46,143 @@ function center_text {
   printf "${YELLOW}${BOLD}${UNDERLINED}%s${NC}\n\n" "$text"
 }
 
+declare -a hundred_numbers
+for i in {1..100}; do
+  new_number=$((RANDOM % 201 - 100))
+  if [[ " ${hundred_numbers[@]} " =~ " ${new_number} " ]]; then
+    continue
+  else
+    hundred_numbers+=($new_number)
+  fi
+done
+IFS=' '
+hundred_numbers_string="\"${hundred_numbers[*]}\""
 
-function leak_indicator {
-	if [ "$?" -ne "0" ]; then
-  echo -e "${RED}${BOLD}Memory leaks detected!${NC}\n"
-else
-  echo -e "${GREEN}${BOLD}No Memory leaks detected!${NC}\n"
-fi
-}
+declare -a five_hundred_numbers
+for i in {1..500}; do
+  new_number=$((RANDOM % 501 - 250))
+  if [[ " ${five_hundred_numbers[@]} " =~ " ${new_number} " ]]; then
+    continue
+  else
+    five_hundred_numbers+=($new_number)
+  fi
+done
+IFS=' '
+five_hundred_numbers_string="\"${hundred_numbers[*]}\""
+
+declare -a five_numbers
+for i in {1..5}; do
+  new_number=$((RANDOM % 25 - 12))
+  if [[ " ${five_numbers[@]} " =~ " ${new_number} " ]]; then
+    continue
+  else
+    five_numbers+=($new_number)
+  fi
+done
+
+#optimize the code to be less cluttered, and also correct the output of false/positives valgrind would display when showing Error
+
+#--------------------------------------------------------------------------------------------------------------------------------
+description=("NO ARGUMENTS" "1 ARGUMENT ONLY" "2 ARGUMENTS SORTED" "3 ARGUMENTS SORTED" "5 ARGUMENTS SORTED" \
+"INT_MIN CHECK" "INT_MAX CHECK" "HAS INVALID CHARS" "HAS DUPLICATES" "2 VALUE STACK" "3 VALUE STACK" "5 VALUE STACK")
+
+args=("" "1" "1 2" "20 21 100" "-3 -1 30 31 100" "3 2 5 -2147483649 3" "3 2 5 2147483648 3" "3 1 -2 c 8" \
+"5 1 4 2 1" "-3 -20" "200 -1 5" "30 -4 72 8 -1")
+
+center_text "NORMAL INPUTS" $(tput cols)
+
+for index in "${!args[@]}"
+do
+    output=$(valgrind --tool=memcheck --leak-check=full ./push_swap ${args[index]} 2>&1)
+    err_output=$(./push_swap ${args[index]} 2>&1 >/dev/null)
+    if echo "$output" | grep -q "Process terminating with default action of signal 11 (SIGSEGV)" && [ -z "$err_output" ]; then
+	 	echo -e "${RED}${BOLD}MEHHH... SEGFAULT!${NC}\n"	
+	elif echo "$output" | grep -q "LEAK SUMMARY:" && [ -z "$err_output" ]; then
+    	echo -e "${RED}${BOLD}Memory leaks detected! with arguments: ${GREY}${args[index]}  (${description[index]}) ${NC}\n"	 
+    elif echo "$output" | grep -q "All heap blocks were freed -- no leaks are possible"; then
+      echo -e "${GREEN}${BOLD}No Memory leaks detected! with arguments: ${GREY}${args[index]} (${description[index]}) ${NC}\n"
+    else
+      echo -e "Unexpected output, check the output manually with arguments: ${GREY}${args[index]}\n"
+    fi
+done
+
+output=$(valgrind --tool=memcheck --leak-check=full ./push_swap ${hundred_numbers_string[@]} 2>&1)
+err_output=$(./push_swap ${hundred_numbers_string[@]} 2>&1 >/dev/null)
+	if echo "$output" | grep -q "Process terminating with default action of signal 11 (SIGSEGV)" && [ -z "$err_output" ]; then
+	 	echo -e "${RED}${BOLD}MEHHH... SEGFAULT!${NC}\n"
+    elif echo "$output" | grep -q "LEAK SUMMARY:" && [ -z "$err_output" ]; then
+      echo -e "${RED}${BOLD}Memory leaks detected! with arguments: ${GREY}100 random numbers   ${NC}\n"
+    elif echo "$output" | grep -q "All heap blocks were freed -- no leaks are possible"; then
+      echo -e "${GREEN}${BOLD}No Memory leaks detected! with arguments: ${GREY}100 random numbers  ${NC}\n"
+    else
+      echo -e "Unexpected output, check the output manually with arguments: ${GREY}${hundred_numbers_string[@]}\n"
+    fi
+
+output=$(valgrind --tool=memcheck --leak-check=full ./push_swap ${five_hundred_numbers_string[@]} 2>&1)
+err_output=$(./push_swap ${five_hundred_numbers_string[@]} 2>&1 >/dev/null)
+	if echo "$output" | grep -q "Process terminating with default action of signal 11 (SIGSEGV)" && [ -z "$err_output" ]; then
+	 	echo -e "${RED}${BOLD}MEHHH... SEGFAULT!${NC}\n"
+    elif echo "$output" | grep -q "LEAK SUMMARY:" && [ -z "$err_output" ]; then
+      echo -e "${RED}${BOLD}Memory leaks detected! with arguments: ${GREY}500 random numbers   ${NC}\n"
+    elif echo "$output" | grep -q "All heap blocks were freed -- no leaks are possible"; then
+      echo -e "${GREEN}${BOLD}No Memory leaks detected! with arguments: ${GREY}500 random numbers  ${NC}\n"
+    else
+      echo -e "Unexpected output, check the output manually with arguments: ${GREY}${five_hundred_numbers_string[@]}\n"
+    fi
 
 
-center_text "NO ARGUMENTS" $(tput cols)
-valgrind --leak-check=yes ./push_swap > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap${NC}\n"
-
-center_text "1 ARGUMENT ONLY" $(tput cols)
-valgrind --leak-check=yes ./push_swap 1 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 1${NC}\n"
-
-center_text "2 ARGUMENTS SORTED" $(tput cols)
-valgrind --leak-check=yes ./push_swap 1 2 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 1 2${NC}\n"
-
-center_text "5 ARGUMENTS SORTED" $(tput cols)
-valgrind --leak-check=yes ./push_swap -3 -1 30 50 100 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap -3 -1 30 50 100${NC}\n"
-
-center_text "INT_MIN CHECK" $(tput cols)
-valgrind --leak-check=yes ./push_swap 3 2 5 -2147483649 3 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 3 2 5 -2147483649 3${NC}\n"
-
-center_text "INT_MAX CHECK" $(tput cols)
-valgrind --leak-check=yes ./push_swap 3 5 2147483648 3 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 3 5 2147483648 3${NC}\n"
-
-center_text "HAS INVALID CHARS" $(tput cols)
-valgrind --leak-check=yes ./push_swap 3 1 -2 c 8 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 3 1 -2 c 8${NC}\n"
-
-center_text "HAS DUPLICATES" $(tput cols)
-valgrind --leak-check=yes ./push_swap 5 1 4 2 1 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 5 1 4 2 1${NC}\n"
-
-center_text "5 ARGUMENTS SORTED" $(tput cols)
-valgrind --leak-check=yes ./push_swap -3 -1 30 50 100 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap -3 -1 30 50 100${NC}\n"
-
-center_text "2 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap -3 -20 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap -3 -20${NC}\n"
-
-center_text "3 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap 200 -1 5 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 200 -1 5${NC}\n"
-
-center_text "5 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap 30 -4 72 8 -1 > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap 30 -4 72 8 -1${NC}\n"
-
-center_text "100 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap ${hundred_numbers[@]} > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap (random 100 numbers)${NC}\n"
-
-center_text "500 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap ${five_hundred_numbers[@]} > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap (random 500 numbers)${NC}\n"
-
-center_text "" $(tput cols)
 center_text "INTS AS SINGLE STRING ARGUMENT" $(tput cols)
-center_text "" $(tput cols)
+#------------------------------------------------------------------------------------------------------------------
+
+description=("1 ARGUMENT ONLY" "2 ARGUMENTS SORTED" "3 ARGUMENTS SORTED" "5 ARGUMENTS SORTED" \
+"INT_MIN CHECK" "INT_MAX CHECK" "HAS INVALID CHARS" "HAS DUPLICATES" "2 VALUE STACK" "3 VALUE STACK" "5 VALUE STACK")
+
+args=('"1"' '"1 2"' '"20 21 100"' '"-3 -1 30 31 100"' '"3 2 5 -2147483649 3"' '"3 2 5 2147483648 3"' '"3 1 -2 c 8"' \
+'"5 1 4 2 1"' '"-3 -20"' '"200 -1 5"' '"30 -4 72 8 -1"')
+
+for index in "${!args[@]}"
+do
+    output=$(valgrind --tool=memcheck --leak-check=full ./push_swap ${args[index]} 2>&1)
+    err_output=$(./push_swap ${args[index]} 2>&1 >/dev/null)
+    if echo "$output" | grep -q "Process terminating with default action of signal 11 (SIGSEGV)" && [ -z "$err_output" ]; then
+	 	echo -e "${RED}${BOLD}MEHHH... SEGFAULT!${NC}\n"	
+	elif echo "$output" | grep -q "LEAK SUMMARY:" && [ -z "$err_output" ]; then
+    	echo -e "${RED}${BOLD}Memory leaks detected! with arguments: ${GREY}${args[index]}  (${description[index]}) ${NC}\n"	 
+    elif echo "$output" | grep -q "All heap blocks were freed -- no leaks are possible"; then
+      echo -e "${GREEN}${BOLD}No Memory leaks detected! with arguments: ${GREY}${args[index]} (${description[index]}) ${NC}\n"
+    else
+      echo -e "Unexpected output, check the output manually with arguments: ${GREY}${args[index]}\n"
+    fi
+done
+
+output=$(valgrind --tool=memcheck --leak-check=full ./push_swap ${hundred_numbers_string[@]} 2>&1)
+err_output=$(./push_swap ${hundred_numbers_string[@]} 2>&1 >/dev/null)
+	if echo "$output" | grep -q "Process terminating with default action of signal 11 (SIGSEGV)" && [ -z "$err_output" ]; then
+	 	echo -e "${RED}${BOLD}MEHHH... SEGFAULT!${NC}\n"
+    elif echo "$output" | grep -q "LEAK SUMMARY:" && [ -z "$err_output" ]; then
+      echo -e "${RED}${BOLD}Memory leaks detected! with arguments: ${GREY}${hundred_numbers_string[@]}${NC}\n"
+    elif echo "$output" | grep -q "All heap blocks were freed -- no leaks are possible"; then
+      echo -e "${GREEN}${BOLD}No Memory leaks detected! with arguments: ${GREY}100 random numbers${NC}\n"
+    else
+      echo -e "Unexpected output, check the output manually with arguments: ${GREY}${hundred_numbers_string[@]}\n"
+    fi
+
+output=$(valgrind --tool=memcheck --leak-check=full ./push_swap ${five_hundred_numbers[@]} 2>&1)
+err_output=$(./push_swap ${five_hundred_numbers[@]} 2>&1 >/dev/null)
+	if echo "$output" | grep -q "Process terminating with default action of signal 11 (SIGSEGV)" && [ -z "$err_output" ]; then
+	 	echo -e "${RED}${BOLD}MEHHH... SEGFAULT!${NC}\n"
+    elif echo "$output" | grep -q "LEAK SUMMARY:" && [ -z "$err_output" ]; then
+      echo -e "${RED}${BOLD}Memory leaks detected! with arguments: ${GREY}500 random numbers   ${NC}\n"
+    elif echo "$output" | grep -q "All heap blocks were freed -- no leaks are possible"; then
+      echo -e "${GREEN}${BOLD}No Memory leaks detected! with arguments: ${GREY}500 random numbers  ${NC}\n"
+    else
+      echo -e "Unexpected output, check the output manually with arguments: ${GREY}${five_hundred_numbers[@]}\n"
+    fi
 
 
-center_text "1 ARGUMENT ONLY" $(tput cols)
-valgrind --leak-check=yes ./push_swap "1" > /dev/null 2>&1
-leak_indicator
-printf "${GREY}./push_swap "'"1"'"${NC}\n"
-
-center_text "2 ARGUMENTS SORTED" $(tput cols)
-valgrind --leak-check=yes ./push_swap "1 2" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"1 2"'"${NC}\n"
-
-center_text "5 ARGUMENTS SORTED" $(tput cols)
-valgrind --leak-check=yes ./push_swap "-3 -1 30 50 100" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"-3 -1 30 50 100"'"${NC}\n"
-
-center_text "INT_MIN CHECK" $(tput cols)
-valgrind --leak-check=yes ./push_swap "3 2 5 -2147483649 3" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"3 2 5 -2147483649 3"'"${NC}\n"
-
-center_text "INT_MAX CHECK" $(tput cols)
-valgrind --leak-check=yes ./push_swap "3 5 2147483648 3" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"3 5 2147483648 3"'"${NC}\n"
-
-center_text "HAS INVALID CHARS" $(tput cols)
-valgrind --leak-check=yes ./push_swap "3 1 -2 c 8" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"3 1 -2 c 8"'"${NC}\n"
-
-center_text "HAS DUPLICATES" $(tput cols)
-valgrind --leak-check=yes ./push_swap "5 1 4 2 1" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"5 1 4 2 1"'"${NC}\n"
-
-center_text "5 ARGUMENTS SORTED" $(tput cols)
-valgrind --leak-check=yes ./push_swap "-3 -1 30 50 100" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"-3 -1 30 50 100"'"${NC}\n"
-
-center_text "2 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap "-3 -20" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"-3 -20"'"${NC}\n"
-
-center_text "3 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap "200 -1 5" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"200 -1 5"'"${NC}\n"
-
-center_text "5 VALUE STACK" $(tput cols)
-valgrind --leak-check=yes ./push_swap "30 -4 72 8 -1" > /dev/null 2>&1
-leak_indicator
-echo -e "${GREY}./push_swap "'"30 -4 72 8"'" -1${NC}\n"
-
-
-
-echo -e "\e[1;30;4;42mERROR MANAGEMENT\e[0m"
+sleep 1
+echo -e "\n\n\e[1;30;4;42mERROR MANAGEMENT\e[0m"
+sleep 1
 
 echo -e "\n${ITALICRED}Run with non numeric parameters${NC}"
 ./push_swap r @ z
@@ -222,9 +197,9 @@ echo -e "\n${ITALICRED}run without parameters ${NC}\n"
 ./push_swap
 
 
-
+sleep 1
 echo -e "\n\e[1;30;4;42mPush_swap - identity test\e[0m\n"
-
+sleep 1
 echo -e "\n${ITALICRED}./push_swap 42 ${NC}"
 ./push_swap 42
 
@@ -241,21 +216,23 @@ if [ ! -f "$checker_name" ]; then
   exit 1
 fi
 
-echo -e "\n\e[1;30;4;42mPush_swap - simple version\e[0m\n"
-
+sleep 1
+echo -e "\n\n\e[1;30;4;42mPush_swap - simple version\e[0m\n"
+sleep 1
 echo -e "\n${ITALICRED}ARG="2 1 0"; ./push_swap $ARG | ./checker $ARG${NC}"
 ARG="2 1 0"; ./push_swap $ARG | ./checker $ARG
 
 
-
-echo -e "\n\e[1;30;4;42mAnother simple version\e[0m\n"
-
+sleep 1
+echo -e "\n\n\e[1;30;4;42mAnother simple version\e[0m\n"
+sleep 1
 echo -e "\n${ITALICRED}ARG="1 5 2 4 3"; ./push_swap $ARG | ./checker $ARG${NC}"
 echo -e "\n"
 ARG="1 5 2 4 3"; ./push_swap $ARG | ./checker $ARG
 echo -e "\n"
 ./push_swap $ARG
 
+sleep 1
 echo -e "\n\n\n${ITALICRED}ARG="${five_numbers[@]}"; ./push_swap ${five_numbers[@]} | ./checker ${five_numbers[@]}${NC}"
 echo -e "\n"
 ARG="${five_numbers[@]}"; ./push_swap ${five_numbers[@]} | ./checker ${five_numbers[@]}
@@ -263,18 +240,18 @@ echo -e "\n"
 ./push_swap ${five_numbers[@]}
 
 
-
+sleep 1
 echo -e "\n\n\e[1;30;4;42mPush_swap - Middle version\e[0m\n"
-
+sleep 1
 echo -e "\n${ITALICRED}ARG="${hundred_numbers[@]}"; ./push_swap 100 numbers | ./checker 100 numbers${NC}"
 echo -e "\n"
 ARG="${hundred_numbers[@]}"; ./push_swap $ARG | ./checker $ARG
 echo -e "\n"
 
 
-
+sleep 1
 echo -e "\n\n\e[1;30;4;42mPush_swap - Advanced version\e[0m\n"
-
+sleep 1
 echo -e "\n${ITALICRED}ARG="${five_hundred_numbers[@]}"; ./push_swap 500 numbers | ./checker 500 numbers${NC}"
 echo -e "\n"
 ARG="${five_hundred_numbers[@]}"; ./push_swap $ARG | ./checker $ARG
